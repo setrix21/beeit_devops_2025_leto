5233fd0390fb93f7ed82592b45c969dca4000177
jsladky <jiri.sladk@seznam.cz>
2025-06-02 14:49:49 +0200

    Rozšíření scriptu

--- linux_cli.sh
+++ linux_cli.sh
@@ -4,3 +4,19 @@ echo "Linux verze:"
 cat /etc/os-release
 
 # dalsicommit
+CILOVA_SLOZKA="adresar/podadresar/posledniadresar/"
+mkdir -p "$CILOVA_SLOZKA"
+
+echo "AHoj ze sveta Linux" > "$CILOVA_SLOZKA/soubor.txt"
+
+ln -sf "$PWD/$CILOVA_SLOZKA/soubor.txt" /tmp/softLink
+
+cp "$CILOVA_SLOZKA/soubor.txt" /tmp/
+
+echo "UID: $(id -u)"
+echo "GID: $(id -g)"
+
+chmod o=r "$CILOVA_SLOZKA/soubor.txt"
+
+echo "chmod nefunguje na symlink"
+
